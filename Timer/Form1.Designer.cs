5d589eb709d43aeb33d2d2a9b7ed2088c40d08b0
Mostik <kirill.mostimenko@gmail.com>
2020-01-22 19:41:09 +0300

    add text by date

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("ObelixPro", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label1.Location = new System.Drawing.Point(252, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(155, 26);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "9994:44:16";
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon2.Icon")));
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            this.notifyIcon2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon2_MouseDoubleClick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ObelixPro", 15.75F);
+            this.label2.Location = new System.Drawing.Point(63, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Все время";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("ObelixPro", 15.75F);
+            this.label4.Location = new System.Drawing.Point(63, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 26);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "24 часа";
+            this.label4.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("ObelixPro", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(252, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 26);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "9994:44:16";
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 60000;
+            this.timer4.Tick += new System.EventHandler(this.Timer4_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(17, 106);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(140, 186);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(217, 186);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(460, 309);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Timers;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+namespace Timer
+{
+
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, String lpString, int nMaxCount); // объявляем метод на C#
+
+        [DllImport("user32.dll")]
+        public static extern UInt32 GetWindowThreadProcessId(IntPtr hwnd, ref Int32 pid);
+
+
+        public static DateTime current_date = DateTime.Now;
+        public static int day;
+        public static int time_day;
+        public static string text_day;
+        public static int time_all;
+        public static string text;
+
+        static RegistryKey currentUserKey = Registry.CurrentUser;
+        static RegistryKey Time = currentUserKey.CreateSubKey("Time");
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (Time.GetValue("time_all") is null)
+                time_all = 0;
+            else
+                time_all = Convert.ToInt32(Time.GetValue("time_all").ToString());
+
+            if (Time.GetValue(current_date.ToShortDateString()) is null)
+            {
+                time_day = 0;
+                text_day = "";
+            }
+            else 
+            {
+                Parsing_Key(Time.GetValue(current_date.ToShortDateString()).ToString());
+                
+            }
+
+            Time.SetValue("time_all", time_all);
+            text = time_day + "!" + textBox1.Text;
+            Time.SetValue(current_date.ToShortDateString(), text);
+            ShowTime();
+            StatDays();
+
+            this.ShowInTaskbar = false;
+
+        }
+
+
+        private void NotifyIcon2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.Visible)
+            {
+
+                this.Hide();
+
+            }
+            else
+            {
+                this.ShowInTaskbar = true;
+                this.Show();
+
+            }
+
+
+        }//-- Открыть/заркыть окно в трее
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }//--
+        private void Timer3_Tick(object sender, EventArgs e)//-- Окно в фокусе время идет
+        {
+            IntPtr h = GetForegroundWindow();
+            int pid = 0;
+            GetWindowThreadProcessId(h, ref pid);
+            Process p = Process.GetProcessById(pid);
+            string text = p.MainWindowTitle;
+            if (text.IndexOf("Visual") > 0 || text.IndexOf("Atom") > 0 || text.IndexOf("Notepad++") > 0)
+            {
+                CheckDate();
+                time_all++;
+                time_day++;
+                ShowTime();
+            }
+            
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)//Окно закрывается сохраняем regedit
+        {
+            Time.SetValue("time_all", time_all);
+            text = time_day + "!" + textBox1.Text;
+            Time.SetValue(current_date.ToShortDateString(), text);
+            Time.Close();
+            notifyIcon2.Visible = false;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Hide();
+        }//--
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }//--
+        private void CheckDate()//Проверяем на новую дату
+        {
+            DateTime datenow = DateTime.Now;
+            if (current_date.ToShortDateString() != datenow.ToShortDateString())
+            {
+                current_date = datenow;
+                text = time_day + "!" + textBox1.Text;
+                Time.SetValue(current_date.ToShortDateString(), text);
+                Time.SetValue("time_all", time_all);
+                time_day = 0;
+                text_day = "";
+                textBox1.Text = "";
+            }
+        }
+        private void ShowTime()//-- Время заносится в секундах а мы выводим в часах минутах секундах
+        {
+            int ta = 10000 * 3600 - time_all;
+            int ha = ta / 3600;
+            int t2a = ta - ha * 3600;
+            int ma = t2a / 60;
+            int sa = t2a - ma * 60;
+
+            int td = time_day;
+            int hd = td / 3600;
+            int t2d = td - hd * 3600;
+            int md = t2d / 60;
+            int sd = t2d - md * 60;
+
+            Label1.Text = ha + ":" + Format(ma) + ":" + Format(sa);
+            label6.Text = Format(hd) + ":" + Format(md) + ":" + Format(sd);
+            notifyIcon2.Text = Format(hd) + ":" + Format(md) + ":" + Format(sd);
+        }
+        private string Format(int t) //Форматируем день что бы имел формат 00:00:00
+        {
+            return (t < 10 ? "0" + t.ToString() : t.ToString());
+        }
+        private void Timer4_Tick(object sender, EventArgs e)//В промежуток времени сохраняем время в localstor
+        {
+            text = time_day + "!" + textBox1.Text;
+            Time.SetValue(current_date.ToShortDateString(), text);
+            Time.SetValue("time_all", time_all);
+            
+            StatDays();
+        }
+        private void StatDays()//Проходимся по всем дням и выводим их в список дней
+        {
+            listBox1.Items.Clear();
+            
+            foreach (string valueName in Time.GetValueNames())
+            {
+                //Все дни кроме ключа time_all
+                if (valueName != "time_all")
+                {
+                    if (valueName != "text") 
+                    {
+                    
+                    Parsing_Key(Time.GetValue(valueName).ToString());
+                    int timelist = Convert.ToInt32(time_day);
+
+                    int td = timelist;
+                    int hd = td / 3600;
+                    int t2d = td - hd * 3600;
+                    int md = t2d / 60;
+                    int sd = t2d - md * 60;
+
+                    listBox1.Items.Add(valueName + " - " + Format(hd) + ":" + Format(md) + ":" + Format(sd));
+                    }
+                    
+                    
+                }
+            }
+        }
+        private void Parsing_Key(string text) 
+        {
+            //StringSplitOptions.RemoveEmptyEntries
+            //Time.GetValue("time_all").ToString()
+            textBox1.Text = "";
+            string[] words = text.Split(new char[] { '!' });
+            for (int i = 0; i < words.Length; i++) 
+            {
+                if (i == 0)
+                {
+                    time_day = Convert.ToInt32(words[i]);
+                }
+                else
+                {  
+                    textBox1.Text += (words[i] + "\n");
+                }
+                    
+            }
+                
+            
+            
+        }
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        } //--
+    }
+}
